9421b3f7cb5998bb101be8d6f6eacecb8d961b55
Dureasa <dddure123@outlook.com>
2024-10-24 20:21:11 +0800

    add control module

--- rtl/loongarch_cpu/loongarch_cpu.srcs/sources_1/new/control.v
+++ rtl/loongarch_cpu/loongarch_cpu.srcs/sources_1/new/control.v
@@ -7,6 +7,50 @@ module control (
     output [5:0]    ctrl_signal
 );
 
-    
+    always @(*) begin
+        case (opcode) 
+            `LU12IW, `PCADDU12I: begin
+                ctrl_signal = 6'b010010;        
+            end
+            `BEQ, `BNE, `BLT, `BGE, `BLTU, `BGEU: begin
+                ctrl_signal = 6'b000010;  
+            end
+            `JIRL: begin
+                ctrl_signal = 6'b010011;               
+            end
+            `B: begin
+                ctrl_signal = 6'b000010;      
+            end
+            `BL: begin
+                ctrl_signal = 6'b010010;      
+            end
+            `AL: begin
+                case (fun1)
+                    `AL_R: begin
+                        ctrl_signal = 6'b010000;      
+                    end 
+                    default: begin
+                        ctrl_signal = 6'b010010;
+                    end
+                endcase
+            end
+            `MEM: begin
+                case (fun1)
+                    `LDW: begin
+                        ctrl_signal = 6'b111010;
+                    end
+                    `STW: begin
+                        ctrl_signal = 6'b000110;
+                    end
+                    default: begin
+                        ctrl_signal = 6'b000000;
+                    end
+                endcase
+            end
+            default: begin
+                ctrl_signal = 6'b000000; 
+            end
+        endcase
+    end
     
 endmodule
