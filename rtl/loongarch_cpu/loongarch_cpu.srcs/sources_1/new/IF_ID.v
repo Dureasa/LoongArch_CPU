24c46e25a3fc8f010d59c4d7627364e1e2b5e72f
Dureasa <dddure123@outlook.com>
2024-10-28 18:41:30 +0800

    update

--- rtl/loongarch_cpu/loongarch_cpu.srcs/sources_1/new/IF_ID.v
+++ rtl/loongarch_cpu/loongarch_cpu.srcs/sources_1/new/IF_ID.v
@@ -4,6 +4,7 @@ module IF_ID(
     input           clk,
     input           rst_n,
     input           IF_ID_clear,
+    input           IF_ID_hold,
     input [31:0]    pc_if,
     input [31:0]    instruction_if,
     output reg [31:0] pc_id,
@@ -17,6 +18,8 @@ module IF_ID(
         end else if (IF_ID_clear) begin
             pc_id <= 32'b0;
             instruction_id <= 32'b0;            
+        end else if (IF_ID_hold) begin
+            // 当IF_ID_hold为真时，保持当前值不变
         end else begin
             pc_id <= pc_if;
             instruction_id <= instruction_if;
